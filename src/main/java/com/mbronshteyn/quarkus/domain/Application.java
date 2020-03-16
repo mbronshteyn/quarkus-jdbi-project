cfb4a081fd494a7f7be5a63aa7a45e861b472ce5
mbronshteyn <michael.bronshteyn@gmail.com>
2020-03-16 05:57:07 -0700

    cleanup

--- src/main/java/com/mbronshteyn/quarkus/domain/Application.java
+++ src/main/java/com/mbronshteyn/quarkus/domain/Application.java
@@ -1,16 +1,10 @@
 package com.mbronshteyn.quarkus.domain;
 
-import com.mbronshteyn.quarkus.dao.UserDao;
-import com.mbronshteyn.quarkus.entity.Address;
-import com.mbronshteyn.quarkus.entity.EmplProj;
-import com.mbronshteyn.quarkus.entity.Employee;
-import com.mbronshteyn.quarkus.entity.Project;
 import com.mbronshteyn.quarkus.entity.User;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 
-import java.util.Calendar;
 import java.util.List;
 
 public class Application {
@@ -57,39 +51,32 @@ public class Application {
 
         users.forEach(System.out::println);
 
-        User userDaoReturn =    jdbi.withExtension(UserDao.class, dao -> {
-            dao.update( new User( 1, "Jake") );
-            return dao.getById( 1L );
-        });
-
-        System.out.println( userDaoReturn );
-
-
-        Address address = new Address();
-        address.setId(1L);
-        address.setCountry("DC");
-        address.setCity("Gotham City");
-        address.setStreet("Arkham street 1");
-        address.setPostCode("12345");
-
-        Employee employee = new Employee();
-        employee.setId(1L);
-        employee.setFirstName("James");
-        employee.setLastName("Gordon");
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(1939, Calendar.MAY, 1);
-
-        employee.setBirthday(new java.sql.Date(calendar.getTime().getTime()));
-        employee.setAddressId(address.getId());
-
-        Project project = new Project();
-        project.setId(1L);
-        project.setTitle("Gotham City Police Department Commissioner");
-
-        EmplProj emplProj = new EmplProj();
-        emplProj.setEmployeeId(employee.getId());
-        emplProj.setProjectId(project.getId());
+        // TODO: work in progress
+//        Address address = new Address();
+//        address.setId(1L);
+//        address.setCountry("DC");
+//        address.setCity("Gotham City");
+//        address.setStreet("Arkham street 1");
+//        address.setPostCode("12345");
+//
+//        Employee employee = new Employee();
+//        employee.setId(1L);
+//        employee.setFirstName("James");
+//        employee.setLastName("Gordon");
+//
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(1939, Calendar.MAY, 1);
+//
+//        employee.setBirthday(new java.sql.Date(calendar.getTime().getTime()));
+//        employee.setAddressId(address.getId());
+//
+//        Project project = new Project();
+//        project.setId(1L);
+//        project.setTitle("Gotham City Police Department Commissioner");
+//
+//        EmplProj emplProj = new EmplProj();
+//        emplProj.setEmployeeId(employee.getId());
+//        emplProj.setProjectId(project.getId());
 
 //        try {
 //            // create table
