0c8dd8bae38eae0aa7e91073c612513a4358f17a
mbronshteyn <michael.bronshteyn@gmail.com>
2020-03-18 14:07:59 -0700

    add example from Quarkus github directory:

---
+++ src/main/java/com/mbronshteyn/quarkus/Fruit.java
@@ -0,0 +1,15 @@
+package com.mbronshteyn.quarkus;
+
+public class Fruit {
+
+    public String name;
+    public String description;
+
+    public Fruit() {
+    }
+
+    public Fruit(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
