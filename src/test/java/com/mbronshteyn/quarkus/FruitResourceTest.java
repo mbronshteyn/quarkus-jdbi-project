517df359d79a60ca72c58fb058ec4112ebb3018a
mbronshteyn <michael.bronshteyn@gmail.com>
2020-03-19 07:45:47 -0700

    fixed tests

--- src/test/java/com/mbronshteyn/quarkus/FruitResourceTest.java
+++ src/test/java/com/mbronshteyn/quarkus/FruitResourceTest.java
@@ -1,16 +1,52 @@
 package com.mbronshteyn.quarkus;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.MediaType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
 
 @QuarkusTest
 public class FruitResourceTest {
 
-//    @Test
-//    public void testFruitsEndpoint() {
-//        given()
-//                .when().get("/fruits/one")
-//                .then()
-//                .statusCode(200);
-//    }
+    @Test
+    public void testList() {
+        given()
+                .when().get("/fruits")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(2),
+                        "name", containsInAnyOrder("Apple", "Pineapple"),
+                        "description", containsInAnyOrder("Winter fruit", "Tropical fruit"));
+    }
 
+    @Test
+    public void testAdd() {
+        given()
+                .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .when()
+                .post("/fruits")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(3),
+                        "name", containsInAnyOrder("Apple", "Pineapple", "Pear"),
+                        "description", containsInAnyOrder("Winter fruit", "Tropical fruit", "Winter fruit"));
+
+        given()
+                .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .when()
+                .delete("/fruits")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(2),
+                        "name", containsInAnyOrder("Apple", "Pineapple"),
+                        "description", containsInAnyOrder("Winter fruit", "Tropical fruit"));
+    }
 }
+
