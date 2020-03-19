fd42b57fc66c20e4871509503d7d70c3fdacb78c
mbronshteyn <michael.bronshteyn@gmail.com>
2020-03-19 13:21:29 -0700

    1. add POST Mockito 2. add mock verifies

--- src/test/java/com/mbronshteyn/quarkus/FruitResourceTest.java
+++ src/test/java/com/mbronshteyn/quarkus/FruitResourceTest.java
@@ -1,5 +1,6 @@
 package com.mbronshteyn.quarkus;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mbronshteyn.quarkus.controller.FruitResource;
 import com.mbronshteyn.quarkus.entity.Fruit;
 import com.mbronshteyn.quarkus.service.FruitService;
@@ -9,11 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.MediaType;
 import java.util.HashSet;
 import java.util.Set;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 
 @QuarkusTest
@@ -22,44 +26,57 @@ public class FruitResourceTest {
     @Inject
     FruitResource resource;
 
-    FruitService fruitService = Mockito.mock(FruitService.class);
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    // create mock service
+    FruitService fruitServiceMock = Mockito.mock(FruitService.class);
+
+    Set<Fruit> fruitList = new HashSet<>();
 
     @BeforeEach
     public void setup() {
-        resource.setService(fruitService);
+        // init data
+        fruitList.add(new Fruit("Apple", "Summer fruit"));
+        fruitList.add(new Fruit("Orange", "Winter fruit"));
+        fruitList.add(new Fruit("Pineapple", "Fall fruit"));
+
+        // inject mocks
+        resource.setService(fruitServiceMock);
     }
 
     @Test
     public void testList() {
 
-        Set<Fruit> fruitList = new HashSet<>();
-        fruitList.add(new Fruit("Apple", "Summer fruit"));
-        fruitList.add(new Fruit("Orange", "Winter fruit"));
-        fruitList.add(new Fruit("Pineapple", "Fall fruit"));
-
-        Mockito.when(fruitService.list()).thenReturn(fruitList);
+        Mockito.when(fruitServiceMock.list()).thenReturn(fruitList);
 
         given()
                 .when().get("/fruits")
                 .then()
                 .statusCode(200)
                 .body("$.size()", is(fruitList.size()));
+
+        verify(fruitServiceMock, times(1)).list();
     }
 
-    // TODO: figure out how to do POST with actual object instead of hardcoding strings
-//    @Test
-//    public void testAdd() {
-//        given()
-//                .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
-//                .header("Content-Type", MediaType.APPLICATION_JSON)
-//                .when()
-//                .post("/fruits")
-//                .then()
-//                .statusCode(200)
-//                .body("$.size()", is(3),
-//                        "name", containsInAnyOrder("Apple", "Pineapple", "Pear"),
-//                        "description", containsInAnyOrder("Winter fruit", "Tropical fruit", "Winter fruit"));
-//
-//    }
+    @Test
+    public void testAdd() throws Exception {
+
+        Fruit pear = new Fruit("Pear", "Winter fruit");
+
+        // we don't need to pass actual Fruit, just an object
+        Mockito.when(fruitServiceMock.add(pear))
+                .thenReturn(fruitList);
+
+        given()
+                .body(objectMapper.writeValueAsString(pear))
+                .header("Content-Type", MediaType.APPLICATION_JSON)
+                .when()
+                .post("/fruits")
+                .then()
+                .statusCode(200)
+                .body("$.size()", is(fruitList.size()));
+
+        verify(fruitServiceMock, times(1)).add(pear);
+    }
 }
 
