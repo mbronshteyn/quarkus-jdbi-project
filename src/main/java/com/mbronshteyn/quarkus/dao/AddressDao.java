3f7e93a4b1a973c2d17f66d5e28536ae1552c753
mbronshteyn <michael.bronshteyn@gmail.com>
2020-03-20 09:50:08 -0700

    cleanup remove objects which are no longer needed

--- src/main/java/com/mbronshteyn/quarkus/dao/AddressDao.java
+++
@@ -1,7 +0,0 @@
-package com.mbronshteyn.quarkus.dao;
-
-import com.mbronshteyn.quarkus.entity.Address;
-import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
-
-@RegisterBeanMapper(Address.class)
-public interface AddressDao extends CrudDao<Address, Long> { }
