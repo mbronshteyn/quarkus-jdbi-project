2c1d184a9b0e0ddd068c59c335b6e8ed670c39bb
mbronshteyn <michael.bronshteyn@gmail.com>
2020-03-24 05:50:00 -0700

    add exception handling

--- src/main/java/com/mbronshteyn/quarkus/controller/FruitResource.java
+++ src/main/java/com/mbronshteyn/quarkus/controller/FruitResource.java
@@ -51,32 +51,37 @@ public class FruitResource {
 
             return Response.ok().entity(responseObject).build();
         } catch (Exception e) {
+            logger.atSevere().log(e.getMessage());
             return Response.serverError().status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
 
-    // TODO: add exception handler
     @GET
     @Path("/{uuid}")
-    public Response findById(@PathParam("uuid") String uuid) throws Exception {
-        logger.atInfo().log("inside GET ONE method");
-        Fruit fruit = service.findById(uuid);
-        if (fruit != null) {
-            ResponseObject responseObject = ResponseObject.builder()
-                    .fruit(fruit)
-                    .msg(SUCCESS)
-                    .build();
-            return Response.ok().type(MediaType.APPLICATION_JSON).entity(responseObject).build();
-        } else {
-            ResponseObject responseObject = ResponseObject.builder()
-                    .msg(NOT_FOUND)
-                    .build();
-            return Response.ok().status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON).entity(responseObject).build();
+    public Response findById(@PathParam("uuid") String uuid) {
+        try {
+            logger.atInfo().log("inside GET ONE method");
+            Fruit fruit = service.findById(uuid);
+            if (fruit != null) {
+                ResponseObject responseObject = ResponseObject.builder()
+                        .fruit(fruit)
+                        .msg(SUCCESS)
+                        .build();
+                return Response.ok().type(MediaType.APPLICATION_JSON).entity(responseObject).build();
+            } else {
+                ResponseObject responseObject = ResponseObject.builder()
+                        .msg(NOT_FOUND)
+                        .build();
+                return Response.ok().status(Response.Status.NOT_FOUND).type(MediaType.APPLICATION_JSON).entity(responseObject).build();
+            }
+        } catch (Exception e) {
+            logger.atSevere().log(e.getMessage());
+            return Response.serverError().status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @POST
-    public Response add(Fruit fruit) throws Exception {
+    public Response add(Fruit fruit) {
         try {
             // allow client to pass uuid
             if (fruit.getUuid() == null) {
@@ -100,36 +105,45 @@ public class FruitResource {
             }
 
             return Response.ok().status(status).entity(responseObject).build();
+
         } catch (Exception e) {
             return Response.serverError()
-                    .status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity(e.getMessage()).build();
+                    .status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
     }
 
     @PUT
-    public Response update(Fruit fruit) throws Exception {
-        int result = service.update(fruit);
+    public Response update(Fruit fruit) {
+        try {
+            int result = service.update(fruit);
 
-        ResponseObject responseObject = ResponseObject.builder()
-                .msg(result == 1 ? SUCCESS : NOT_UPDATED)
-                .build();
+            ResponseObject responseObject = ResponseObject.builder()
+                    .msg(result == 1 ? SUCCESS : NOT_UPDATED)
+                    .build();
 
-        return Response.ok()
-                .entity(responseObject)
-                .build();
+            return Response.ok()
+                    .entity(responseObject)
+                    .build();
+        } catch (Exception e) {
+            return Response.serverError()
+                    .status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @DELETE
     @Path("/{uuid}")
-    public Response delete(@PathParam("uuid") String uuid) throws Exception {
+    public Response delete(@PathParam("uuid") String uuid) {
+        try {
+            int result = service.delete(uuid);
+            ResponseObject responseObject = ResponseObject.builder()
+                    .msg(result == 1 ? SUCCESS : NOT_DELETED)
+                    .build();
 
-        int result = service.delete(uuid);
-        ResponseObject responseObject = ResponseObject.builder()
-                .msg(result == 1 ? SUCCESS : NOT_DELETED)
-                .build();
-
-        return Response.ok().entity(responseObject)
-                .build();
+            return Response.ok().entity(responseObject)
+                    .build();
+        } catch (Exception e) {
+            return Response.serverError()
+                    .status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
