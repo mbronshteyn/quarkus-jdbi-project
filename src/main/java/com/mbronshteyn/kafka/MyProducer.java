532985034d546f1b9c751c6345e36f8232901480
mbronshteyn <michael.bronshteyn@gmail.com>
2020-03-29 09:33:02 -0700

    add delay to allow threads to finish

--- src/main/java/com/mbronshteyn/kafka/MyProducer.java
+++ src/main/java/com/mbronshteyn/kafka/MyProducer.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.Properties;
@@ -16,30 +15,42 @@ public class MyProducer {
         props.put("acks", "all");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("enable.auto.commit", "true");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
 
         Flux.range(0, 100)
                 .parallel()
+                .runOn(Schedulers.parallel())
                 .subscribe(i -> {
-                    producer.send(
-                            new ProducerRecord<String, String>(
-                                    "my-topic", Integer.toString(i), Integer.toString(i)));
-                });
+                            System.out.println("Thread " + Thread.currentThread().getId());
+                            producer.send(
+                                    new ProducerRecord<String, String>(
+                                            "my-topic", Integer.toString(i), Integer.toString(i)));
+                        }, (error) -> {
+                        },
+                        () -> {
+                            System.out.println("Finished array");
+                            producer.close();
+                        });
 
-        Mono.just("message from mono")
-                .publishOn(Schedulers.single())
-                .subscribe(message -> {
-                    System.out.println("Sending " + message);
-                    producer.send(
-                            new ProducerRecord<String, String>(
-                                    "my-topic", "xyz", message));
-                }, error -> {
-                }, () -> {
-                    producer.close();
-                });
+//        Mono.just("message from mono")
+//                .publishOn(Schedulers.single())
+//                .subscribe(message -> {
+//                    System.out.println("Sending " + message);
+//                    System.out.println("Thread " + Thread.currentThread().getId() );
+//                    producer.send(
+//                            new ProducerRecord<String, String>(
+//                                    "my-topic", "xyz", message));
+//                }, error -> {
+//                }, () -> {
+//                    System.out.println("Closing producer");
+//                    producer.close();
+//                });
 
         System.out.println("Before or after");
+
+        Thread.sleep(10000);
     }
+
+
 }
