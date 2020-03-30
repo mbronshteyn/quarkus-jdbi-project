9042e045d98d33241bdb8c1b8b173d421f4efaf2
mbronshteyn <michael.bronshteyn@gmail.com>
2020-03-30 07:40:51 -0700

    update demo class

--- src/main/java/com/mbronshteyn/kafka/MyProducer.java
+++ src/main/java/com/mbronshteyn/kafka/MyProducer.java
@@ -3,7 +3,7 @@ package com.mbronshteyn.kafka;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.Properties;
@@ -18,34 +18,43 @@ public class MyProducer {
 
         Producer<String, String> producer = new KafkaProducer<>(props);
 
-        Flux.range(0, 100)
-                .parallel()
-                .runOn(Schedulers.parallel())
-                .subscribe(i -> {
-                            System.out.println("Thread " + Thread.currentThread().getId());
-                            producer.send(
-                                    new ProducerRecord<String, String>(
-                                            "my-topic", Integer.toString(i), Integer.toString(i)));
-                        }, (error) -> {
-                        },
-                        () -> {
-                            System.out.println("Finished array");
-                            producer.close();
-                        });
+//        for( int i = 0; i < 100; i++ ){
+//            producer.send(
+//                    new ProducerRecord<String, String>(
+//                            "my-topic", "abc", Integer.toString(i)));
+//        }
+//
+//        producer.close();
 
-//        Mono.just("message from mono")
-//                .publishOn(Schedulers.single())
-//                .subscribe(message -> {
-//                    System.out.println("Sending " + message);
-//                    System.out.println("Thread " + Thread.currentThread().getId() );
-//                    producer.send(
-//                            new ProducerRecord<String, String>(
-//                                    "my-topic", "xyz", message));
-//                }, error -> {
-//                }, () -> {
-//                    System.out.println("Closing producer");
-//                    producer.close();
-//                });
+//        Flux.range(0, 100)
+//                .parallel()
+//                .runOn(Schedulers.parallel())
+//                .subscribe(i -> {
+//                            System.out.println("Thread " + Thread.currentThread().getId());
+//                            producer.send(
+//                                    new ProducerRecord<String, String>(
+//                                            "my-topic", Integer.toString( i ), Integer.toString(i)));
+//                        }, (error) -> {
+//                        },
+//                        () -> {
+//                            System.out.println("Finished array");
+//                            producer.close();
+//                        });
+
+        System.out.println("Current thread: " + Thread.currentThread().getId());
+        Mono.just("message from mono")
+                .publishOn(Schedulers.single())
+                .subscribe(message -> {
+                    System.out.println("Sending " + message);
+                    System.out.println("Thread " + Thread.currentThread().getId());
+                    producer.send(
+                            new ProducerRecord<String, String>(
+                                    "my-topic", "xyz", message));
+                }, error -> {
+                }, () -> {
+                    System.out.println("Closing producer");
+                    producer.close();
+                });
 
         System.out.println("Before or after");
 
