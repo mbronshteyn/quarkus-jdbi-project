f886f1326998b88dc75b9ddddb9dac8394a77724
mbronshteyn <michael.bronshteyn@gmail.com>
2020-04-18 08:49:24 -0700

    show how to run on multiple threads

--- src/main/java/com/mbronshteyn/kafka/MyProducer.java
+++ src/main/java/com/mbronshteyn/kafka/MyProducer.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import java.util.Properties;
 
@@ -18,49 +17,49 @@ public class MyProducer {
 
         Producer<String, String> producer = new KafkaProducer<>(props);
 
-//        for( int i = 0; i < 100; i++ ){
-//            producer.send(
-//                    new ProducerRecord<String, String>(
-//                            "my-topic", "abc", Integer.toString(i)));
-//        }
-//
-//        producer.close();
+        //        for( int i = 0; i < 100; i++ ){
+        //            producer.send(
+        //                    new ProducerRecord<String, String>(
+        //                            "my-topic", Integer.toString(i), Integer.toString(i)));
+        //        }
+        //
+        //        producer.close();
 
-//        Flux.range(0, 100)
-//                .parallel()
-//                .runOn(Schedulers.parallel())
-//                .subscribe(i -> {
-//                            System.out.println("Thread " + Thread.currentThread().getId());
-//                            producer.send(
-//                                    new ProducerRecord<String, String>(
-//                                            "my-topic", Integer.toString( i ), Integer.toString(i)));
-//                        }, (error) -> {
-//                        },
-//                        () -> {
-//                            System.out.println("Finished array");
-//                            producer.close();
-//                        });
+        //        Flux.range(0, 100)
+        //                .parallel()
+        //                .runOn(Schedulers.parallel())
+        //                .subscribe(i -> {
+        //                            System.out.println("Thread " + Thread.currentThread().getId());
+        //                            producer.send(
+        //                                    new ProducerRecord<String, String>(
+        //                                            "my-topic", Integer.toString( i ),
+        // Integer.toString(i)));
+        //                        }, (error) -> {
+        //                        },
+        //                        () -> {
+        //                            System.out.println("Finished array");
+        //                            producer.close();
+        //                        });
 
         System.out.println("Current thread: " + Thread.currentThread().getId());
         Mono.just("message from mono")
-                .publishOn(Schedulers.single())
-                .subscribe(message -> {
-                    System.out.println("Sending " + message);
-                    System.out.println("Thread " + Thread.currentThread().getId());
-                    producer.send(
-                            new ProducerRecord<String, String>(
-                                    "my-topic", "xyz", message));
-                }, error -> {
-                    // nothing for now
-                }, () -> {
-                    System.out.println("Closing producer");
-                    producer.close();
-                });
+                //              .publishOn(Schedulers.single())  uncomment to run on separate thread
+                .subscribe(
+                        message -> {
+                            System.out.println("Sending " + message);
+                            System.out.println("Thread " + Thread.currentThread().getId());
+                            producer.send(new ProducerRecord<String, String>("my-topic", "xyz", message));
+                        },
+                        error -> {
+                            // nothing for now
+                        },
+                        () -> {
+                            System.out.println("Closing producer");
+                            producer.close();
+                        });
 
         System.out.println("Before or after");
 
         Thread.sleep(10000);
     }
-
-
 }
