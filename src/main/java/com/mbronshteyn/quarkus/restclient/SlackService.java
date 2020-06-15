c7b9e92a17fbce1d1aaaf1bed46cc39edf976ed3
mbronshteyn <michael.bronshteyn@gmail.com>
2020-06-15 08:42:49 -0700

    try to change properties

--- src/main/java/com/mbronshteyn/quarkus/restclient/SlackService.java
+++ src/main/java/com/mbronshteyn/quarkus/restclient/SlackService.java
@@ -4,11 +4,13 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.inject.Singleton;
 import javax.ws.rs.POST;
-
+import javax.ws.rs.Path;
 
 @Singleton
+@Path(("/services"))
 @RegisterRestClient
 public interface SlackService {
+  @Path("/T014U3P9M4P/B0160MMCUPJ/Im19VNFCDetzrtomQSsuCUvV")
   @POST
   void postMessage(String text);
 }
