c81e4aee14f7b936d8f65f01b0df06369122dc74
mbronshteyn <michael.bronshteyn@gmail.com>
2020-08-13 09:37:04 -0700

    updates

--- src/main/java/com/mbronshteyn/helpers/ReactiveSandbox.java
+++ src/main/java/com/mbronshteyn/helpers/ReactiveSandbox.java
@@ -2,10 +2,15 @@ package com.mbronshteyn.helpers;
 
 import io.reactivex.Flowable;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ReactiveSandbox {
   public static void main(String[] args) throws Exception {
+
+    List<String> list = Arrays.asList( "1", "2", "3", "4", "5", "6", "7", "8"  );
 
     System.out.println("Checkpoint #1 on " + Thread.currentThread().getName() + "\n\n");
 //    Flux.range(1, 10)
@@ -15,16 +20,17 @@ public class ReactiveSandbox {
 
     System.out.println("Checkpoint #2 on " + Thread.currentThread().getName() + "\n\n");
 
-//    Observable.just(1)
+//    Observable.fromIterable( list )
 //            .flatMap(
 //                    e ->
 //                            Observable.just(e)
-//                                    .subscribeOn(io.reactivex.schedulers.Schedulers.computation())
+//                                    .subscribeOn(io.reactivex.schedulers.Schedulers.from( Executors.newScheduledThreadPool(8) ))
 //                                    .map(ReactiveSandbox::intenseCalculation))
 //            .subscribe();
 
-    Flowable.just(getRecord())
-            .subscribeOn(io.reactivex.schedulers.Schedulers.computation())
+//    Flowable.just(getRecord())
+    Flowable.fromIterable(  list )
+            .subscribeOn(io.reactivex.schedulers.Schedulers.from( Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() )))
             .subscribe(ReactiveSandbox::intenseCalculation);
 
     System.out.println("Checkpoint #3 on " + Thread.currentThread().getName() + "\n\n");
@@ -32,21 +38,16 @@ public class ReactiveSandbox {
 
     Thread.sleep(10000);
   }
-
   public static int getRecord() {
     System.out.println("Insidie getRecord: thread name: " + Thread.currentThread().getName());
     return 18;
   }
 
   // process the value
-  public static int intenseCalculation(int i) {
-    try {
+  public static String intenseCalculation( String i ) {
       System.out.println("Calculating " + i + " on " + Thread.currentThread().getName());
-      Thread.sleep(1000);
+//      Thread.sleep(1000);
       return i;
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
   }
 
   public static int randInt(int min, int max) {
